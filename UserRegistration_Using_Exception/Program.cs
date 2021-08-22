c89b640d1401b74ebe342aa0042519cbf10cede4
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-22 14:08:09 +0530

    feat:Welcome To User Registration Using Exception

---
+++ UserRegistration_Using_Exception/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration_Using_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Using Exception !");
+        }
+    }
+}
